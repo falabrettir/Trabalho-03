7939bbfd9b7222f01ef7717b0e3bcce8c93c6314
Eduardo Dias <eduardoviniciusdias@alunos.utfpr.edu.br>
2023-11-27 12:42:42 -0300

    rotulo

--- t3-pacote/t3-2602644-2602709-2401231.c
+++ t3-pacote/t3-2602644-2602709-2401231.c
@@ -1,16 +1,23 @@
 #include "trabalho3.h"
 #include "imagem.h"
 #include <math.h>
+#include <stdlib.h>
+
+#define MIN2(a, b) (a < b ? a:b)
+#define MIN3(a, b, c) (MIN2(MIN2(a, b), c))
+#define MIN4(a, b, c, d) (MIN2(MIN3(a, b, c), d))
 
 double calculaAngulo(Coordenada l, Coordenada r);
 Coordenada* achaCentros(Imagem1C img);
 double detectaSensorBar(Imagem1C* img, Coordenada* l, Coordenada* r);
 void diminuiRuido(Imagem1C* img);
+int** rotulaMatriz(Imagem1C* img);
 
 
 double detectaSensorBar(Imagem1C* img, Coordenada* l, Coordenada* r)
 {
-    //diminuiRuido(img);
+    diminuiRuido(img);
+    rotulaMatriz(img);
     return 0.0;
 }
 
@@ -19,17 +26,16 @@ double calculaAngulo(Coordenada l, Coordenada r)
     return 0.0;
 }
 
-Coordenada* achaCentros(Imagem1C img)
+Coordenada* achaCentro(Imagem1C img) // Função bolacha do mar
 {
-    // Acha o centro de uma bola
+    
 }
 
 void diminuiRuido(Imagem1C* img)
 {
     int i, j, soma, media;
-    int sentidox, sentidoy;
+    Imagem1C copia = *img;
 
-    sentidox
     for (i = 1; i < img->altura-1; i++)
     {
         for (j = 1; j < img->largura-1; j++)
@@ -39,11 +45,56 @@ void diminuiRuido(Imagem1C* img)
 
             media = soma/150;
 
-            if(media < 80)
-                img->dados[i][j] = 0;
+            if(media < 100)
+                copia.dados[i][j] = 0;
             else
-                img->dados[i][j] = 255;
-            
+                copia.dados[i][j] = 255;
+        }
+        img->dados = copia.dados;
+    }
+}
+
+int** rotulaMatriz(Imagem1C* img)
+{
+    int i, j;
+    int rotulo = 1;
+    int **matriz_rotulada;
+
+    matriz_rotulada = (int**) malloc(sizeof(int*) * img->altura);
+    for (i = 0; i < img->largura; i++)
+        matriz_rotulada[i] = (int*) malloc(sizeof(int) * img->largura);
+
+    for (i = 0; i < img->altura; i++)
+    {
+        for (j = 0; j < img->largura; j++)
+        {
+            matriz_rotulada[i][j] = 0;
+        }
+    
+
+    for (i = 1; i < img->altura - 1; i++)
+    {
+        for (j = 1; j < img->largura - 1; j++)
+        {
+            if (img->dados[i][j])
+            {
+                if (!(img->dados[i][j-1] + img->dados[i-1][j-1] + img->dados[i-1][j] + img->dados[i-1][j+1]))
+                {
+                    matriz_rotulada = rotulo++;
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+
+    for (i = img->altura - 2; i > 0; i--)
+    {
+        for (j = img->largura - 2; j > 0 ; j--)
+        {
+
         }
     }
 }
