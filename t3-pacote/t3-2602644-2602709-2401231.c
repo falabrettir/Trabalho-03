08914a9d9ced1b20988f2a8bb508360622ffb560
Eduardo Dias <eduardoviniciusdias@alunos.utfpr.edu.br>
2023-12-03 12:24:30 -0300

    Update t3-2602644-2602709-2401231.c

--- t3-pacote/t3-2602644-2602709-2401231.c
+++ t3-pacote/t3-2602644-2602709-2401231.c
@@ -1,137 +1,125 @@
-/*============================================================================*/
-/* TRABALHO: WII SENSOR BAR                                                   */
-/*----------------------------------------------------------------------------*/
-/* Autor: Bogdan T. Nassu                                                     */
-/*============================================================================*/
-
-#include <stdio.h>
-#include <time.h>
-#include <math.h>
-#include "gerador_de_testes.h"
 #include "trabalho3.h"
+#include "imagem.h"
+#include <math.h>
+#include <stdlib.h>
 
-/*============================================================================*/
-/* Um modificador para a semente usada pelo gerador de n�meros aleat�rios na
- * hora de criar imagens de teste. Todos os trabalhos usar�o as mesmas
- * imagens, ent�o o offset precisa ser igual para todos. Entretanto, usaremos
- * um valor diferente na hora de testar os trabalhos de fato. */
+#define MAX2(a, b) (a > b ? a:b)
+#define MAX3(a, b, c) (MAX2(MAX2(a, b), c))
+#define MAX4(a, b, c, d) (MAX2(MAX3(a, b, c), d))
 
-#define RANDOM_SEED_OFFSET 0
+double calculaAngulo(Coordenada l, Coordenada r);
+Coordenada* achaCentros(Imagem1C img);
+double detectaSensorBar(Imagem1C* img, Coordenada* l, Coordenada* r);
+void diminuiRuido(Imagem1C* img);
+int** rotulaMatriz(Imagem1C* img);
+int menorDiferenteDeZero(int a, int b, int c, int d);
 
-#define N_TESTES 1
 
-#define SALVA_INTERMEDIARIOS 1 /* Flag que diz se devemos salvar as imagens de teste. Desative se for rodar muitos testes! */
-
-/*============================================================================*/
-
-#define MIN(a,b) ((a<b)? a : b)
-#define MAX(a,b) ((a>b)? a : b)
-
-/*============================================================================*/
-
-int main ()
+double detectaSensorBar(Imagem1C* img, Coordenada* l, Coordenada* r)
 {
+    int** matriz_rotulada;
     int i;
-    Imagem1C* img; /* Imagem de teste. */
-    clock_t tempo_inicio, tempo_total = 0;
-    double erro_l, erro_r;
-    double erros [N_TESTES];
-    double erro_medio, desvpad, erro_max;
-    int pior_teste = -1;
-    /* TODO: depois da entrega! int angulos_errados = 0; */
-    Coordenada l_real, r_real, l_medido, r_medido;
-    double diagonal, angulo_real, angulo_medido;
 
-    /* Realiza N_TESTES testes. */
-    for (i = 0; i < N_TESTES; i++)
-    {
-        printf ("----------------------------------------\n");
-        printf ("Teste %d\n", i);
+    diminuiRuido(img);
+    
+    /* matriz_rotulada = rotulaMatriz(img);
 
-        /* Gera um caso de teste. */
-        img = geraCasoDeTeste (i+RANDOM_SEED_OFFSET, &l_real, &r_real, &angulo_real);
-        if (!img)
-        {
-            printf ("Erro gerando caso de teste (avisar o professor!!!).\n");
-            return (1);
-        }
-        diagonal = sqrt (img->altura*img->altura + img->largura*img->largura);
-
-        /* Salva (se necess�rio). 
-        if (SALVA_INTERMEDIARIOS)
-        {
-            char foostring [64];
-            sprintf (foostring, "teste%d.bmp", i);
-            salvaImagem1C (img, foostring);
-        }*/
-
-        /* Invoca o testador. */
-        tempo_inicio = clock ();
-        angulo_medido = detectaSensorBar (img, &l_medido, &r_medido);
-        tempo_total += clock () - tempo_inicio;
-
-        /* Salva (se necess�rio). */
-        if (SALVA_INTERMEDIARIOS)
-        {
-            char foostring [64];
-            sprintf (foostring, "teste%d.bmp", i);
-            salvaImagem1C (img, foostring);
-        }
-
-        /* Compara os resultados com o que foi gerado. */
-        /*if (!isfinite (l_medido.x) || !isfinite (l_medido.y) || !isfinite (r_medido.x) || !isfinite (r_medido.y))
-            erros [i] = diagonal;
-        else
-        {
-            erro_l = sqrt ((l_real.x-l_medido.x)*(l_real.x-l_medido.x) + (l_real.y-l_medido.y)*(l_real.y-l_medido.y));
-            erro_r = sqrt ((r_real.x-r_medido.x)*(r_real.x-r_medido.x) + (r_real.y-r_medido.y)*(r_real.y-r_medido.y));
-            erros [i] = MIN (diagonal, MAX (erro_l, erro_r));
-        }*/
-        //TODO: somente depois da entrega!
-        /*
-        if (fabs (angulo_real - angulo_medido) > 0.0002)
-            angulos_errados++;
-        printf ("Real: (%d,%d) e (%d,%d) (%.4f rad)\nDetectado: (%d,%d) e (%d,%d) (%.4f rad)\nErro: %.2f\n", l_real.x, l_real.y, r_real.x, r_real.y, angulo_real, l_medido.x, l_medido.y, r_medido.x, r_medido.y, angulo_medido, erros [i]);
-        */
-
-        printf ("Real: (%d,%d) e (%d,%d) (SEGREDO rad)\nDetectado: (%d,%d) e (%d,%d) (%.4f rad)\nErro: %.2f\n", l_real.x, l_real.y, r_real.x, r_real.y, l_medido.x, l_medido.y, r_medido.x, r_medido.y, angulo_medido, erros [i]);
-
-        /* Joga fora as imagens geradas. */
-        destroiImagem1C (img);
-    }
-
-    /* Calcula m�dia, m�ximo e desvio padr�o. */
-    erro_medio = erros [0];
-    erro_max = erros [0];
-    pior_teste = 0;
-    for (i = 1; i < N_TESTES; i++)
-    {
-        erro_medio += erros [i];
-        if (erros [i] > erro_max)
-        {
-            erro_max = erros [i];
-            pior_teste = i;
-        }
-    }
-
-    erro_medio /= N_TESTES;
-
-    desvpad = 0;
-    for (i = 0; i < N_TESTES; i++)
-        desvpad += (erros [i] - erro_medio)*(erros [i] - erro_medio);
-    desvpad /= N_TESTES;
-    desvpad = sqrt (desvpad);
-
-
-    printf ("----------------------------------------\n");
-    printf ("Erro medio: %.6f\n", erro_medio);
-    printf ("Pior teste: %d (erro: %.10ff)\n", pior_teste, erro_max);
-    printf ("Desvio padrao: %.6f\n", desvpad);
-    printf ("Score: %.6f\n", erro_medio + 1.5*desvpad);
-    // TODO: depois da entrega! printf ("Angulos errados: %d\n", angulos_errados);
-    printf ("Tempo total: %d\n", (int) tempo_total);
-
-    return (0);
+    for (i = 0; i < img->altura; i++)
+        free(matriz_rotulada[i]);
+    free(matriz_rotulada);*/
+    
+    return 0.0;
 }
 
-/*============================================================================*/
+void diminuiRuido(Imagem1C* img)
+{
+    int i, j, soma, media;
+    Imagem1C copia = *img;
+
+    for (i = 1; i < img->altura-1; i++)
+    {
+        for (j = 1; j < img->largura-1; j++)
+        {
+            soma = img->dados[i][j+1]*20 + img->dados[i][j-1]*20 + img->dados[i+1][j]*20 + img->dados[i-1][j]*20 + img->dados[i][j]*30 + 
+                   img->dados[i+1][j-1]*10 + img->dados[i+1][j+1]*10 + img->dados[i-1][j-1]*10 + img->dados[i-1][j+1]*10;
+
+            media = soma/150;
+
+            if(media < 120)
+                copia.dados[i][j] = 0;
+            else
+                copia.dados[i][j] = 255;
+        }
+        img->dados = copia.dados;
+    }
+}
+
+int** rotulaMatriz(Imagem1C* img)
+{
+    int i, j;
+    int rotulo, soma_vizinhanca;
+    int **matriz_rotulada;
+
+    matriz_rotulada = (int**) malloc(sizeof(int*) * img->altura);
+    for (i = 0; i < img->largura; i++)
+        matriz_rotulada[i] = (int*) malloc(sizeof(int) * img->largura);
+
+    for (i = 0; i < img->altura; i++)
+    {
+        for (j = 0; j < img->largura; j++)
+        {
+            if (img->dados[i][j])
+                matriz_rotulada[i][j] = -1;
+        }
+    }
+
+    rotulo = 1;
+    for (i = 1; i < img->altura - 1; i++)
+    {
+        for (j = 1; j < img->largura - 1; j++)
+        {
+            if (img->dados[i][j])
+            {
+                soma_vizinhanca = img->dados[i][j-1] + img->dados[i-1][j-1] + img->dados[i-1][j] + img->dados[i-1][j+1];
+                if (!soma_vizinhanca)
+                {
+                    matriz_rotulada[i][j] = rotulo++;
+                }
+                else
+                {
+                    matriz_rotulada[i][j] = menorDiferenteDeZero(matriz_rotulada[i][j-1], matriz_rotulada[i-1][j-1], matriz_rotulada[i-1][j], matriz_rotulada[i-1][j+1]);
+                }
+            }
+        }
+    }
+
+    for (i = img->altura - 2; i > 0; i--)
+    {
+        for (j = img->largura - 2; j > 0 ; j--)
+        {
+            if (matriz_rotulada[i][j])
+            {
+                soma_vizinhanca = matriz_rotulada[i][j+1] + matriz_rotulada[i+1][j+1] + matriz_rotulada[i+1][j] + matriz_rotulada[i+1][j-1];
+                if (soma_vizinhanca)
+                    {
+                        matriz_rotulada[i][j] = menorDiferenteDeZero(matriz_rotulada[i][j+1], matriz_rotulada[i+1][j+1], matriz_rotulada[i+1][j], matriz_rotulada[i+1][j-1]);
+                    }
+            }
+        }
+    }
+    return matriz_rotulada;
+}
+
+int menorDiferenteDeZero(int a, int b, int c, int d)
+{
+    int i;
+    int vals[] = {a, b, c, d};
+    int menor;
+    menor = MAX4(a, b, c, d);
+    for (i = 0; i < 4; i++)
+    {
+        if (vals[i] && (vals[i] < menor))
+            menor = vals[i];
+    }
+    return menor;
+}
